20daa428d31f009d03abab77a2e4aae72818348e
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2026-04-19 20:31:44 +0600

    create json file if doesn't exist at startup

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+from pathlib import Path
+import json
+
+file_path = Path("data.json")
+
 def main():
-    while True:
-        pass
+    if not file_path.is_file():
+        print("data.json not found. Creating data.json...")
+
+        with open(file_path, "w", encoding="utf-8") as f:
+            # ensure_ascii=False to allow non-ASCII characters to be written properly
+            json.dump({}, f, indent=4, ensure_ascii=False)
+    # while True:
+    #     pass
 
 if __name__ == "__main__":
     main()
