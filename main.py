83745b5a782a91ce084a395f8b08f88c9a08e8cd
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2026-04-20 16:24:50 +0600

    add TodoItem class

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from pathlib import Path
 import json
+import uuid
 
 file_path = Path("data.json")
+
+class TodoItem:
+    def __init__(self, title: str, description: str = "", completed: bool = False):
+        self.id = str(uuid.uuid7())
+        self.title = title
+        self.description = description
+        self.completed = completed
 
 def main():
     if not file_path.is_file():
@@ -10,8 +18,10 @@ def main():
         with open(file_path, "w", encoding="utf-8") as f:
             # ensure_ascii=False to allow non-ASCII characters to be written properly
             json.dump({}, f, indent=4, ensure_ascii=False)
-    # while True:
-    #     pass
+    else:
+        while True:
+            print("Showing todo list:")
+
 
 if __name__ == "__main__":
     main()
