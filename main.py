a8fb1eccfc65b1f7402daf1eb0a4c5c1ddf58f4f
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2026-04-21 20:34:41 +0600

    implemented add todo feature

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ class TodoItem:
         self.description = description
         self.status = status
 
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "status": self.status
+        }
+
 def display_todo_item(todo: TodoItem):
     print(f"ID: {todo['id']}")
     print(f"Title: {todo['title']}")
@@ -26,6 +34,19 @@ def display_todo_list(todos: list[TodoItem]):
     for todo in todos:
         display_todo_item(todo)
 
+def add_todo(title: str, description: str, status:str = "todo"):
+    if title:
+        item = TodoItem(title, description, status)
+        
+        with open(file_path, "r", encoding="utf-8") as f:
+            todos = json.load(f)
+        
+        todos.append(item.to_dict())
+
+        with open(file_path, "w", encoding="utf-8") as f:
+            json.dump(todos, f, indent=4, ensure_ascii=False)
+
+
 def main():
     if not file_path.is_file():
         print("data.json not found. Creating data.json...")
@@ -38,6 +59,17 @@ def main():
         with open(file_path, "r", encoding="utf-8") as f:
             todos = json.load(f)
             display_todo_list(todos)
+        terminate = False
+        while not terminate:
+            user_input = input("Enter 'add' to add a new todo item or 'exit' to quit: ").lower()
+            if user_input == "exit":
+                terminate = True
+                print("Exiting...")
+            elif user_input == "add":
+                new_item = input("Enter Title and Description separated by | e.g. first item title|first item description:\n")
+                title_description = new_item.split(sep="|")
+                add_todo(title=title_description[0], description=title_description[1], status="todo")
+
 
 
 if __name__ == "__main__":
